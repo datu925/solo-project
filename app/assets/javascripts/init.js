7b753fb0c79928c87835fe924561ba9ec8229a11
Dan Turcza <email": null>
2016-03-05 19:40:46 -0600

    Add code for tutorials 1, 2, and 3

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,10 +1,64 @@
 $(function() {
 
-  var data = [4, 8, 15, 16, 23, 42];
-  d3.select(".chart")
-  .selectAll("div")
-    .data(data)
-  .enter().append("div")
-    .style("width", function(d) { return d * 10 + "px"; })
-    .text(function(d) { return d; });
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+        width = 960 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], 0.1)
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(10, "%");
+
+    var chart = d3.select(".chart")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  d3.csv("/data.csv", type, function(error, data) {
+    x.domain(data.map(function(d) { return d.letter; }));
+    y.domain([0, d3.max(data, function(d) { return d.frequency; })]);
+
+    var barWidth = width / data.length
+    // debugger
+    chart.append("g")
+         .attr("class", "x axis")
+         .attr("transform", "translate(0," + height + ")")
+         .call(xAxis);
+
+    chart.append("g")
+       .attr("class", "y axis")
+       .call(yAxis)
+     .append("text")
+       .attr("transform", "rotate(-90)")
+       .attr("y", 6)
+       .attr("dy", ".71em")
+       .style("text-anchor", "end")
+       .text("Frequency");
+
+
+    chart.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.letter); })
+      .attr("y", function(d) { return y(d.frequency); })
+      .attr("height", function(d) { return height - y(d.frequency); })
+      .attr("width", x.rangeBand());
+  });
+
+  function type(d) {
+    d.frequency = +d.frequency; // coerce to number
+    return d;
+  }
 });
