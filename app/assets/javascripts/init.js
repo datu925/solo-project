c37cd47a9e7eecb2304cee22ae9177f9bd0c88a5
Dan Turcza <datu925@gmail.com>
2016-03-06 23:46:27 -0600

    Add nhl and states visualizations

--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,57 +1,120 @@
 $(function() {
 
-    var margin = {top: 20, right: 30, bottom: 30, left: 40},
-        width = 960 - margin.left - margin.right,
-        height = 700 - margin.top - margin.bottom;
+    var outerRadius = 960 / 2,
+        innerRadius = outerRadius - 180;
 
-
-    var chart = d3.select(".chart")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    var svg = d3.select("body").append("svg")
+        .attr("width", outerRadius * 2)
+        .attr("height", outerRadius * 2)
       .append("g")
-        .attr("transform", "translate(" + (width / 2 + margin.left) + "," + (height / 2 + margin.top) + ")");
+        .attr("transform", "translate(" + outerRadius + "," + outerRadius + ")");
 
-    var maxRadius = 70;
+    var fill = d3.scale.category20c();
+
+    var chord = d3.layout.chord()
+        .padding(.01)
+        // .sortGroups(d3.descending);
+        // .sortSubgroups(d3.descending)
+        // .sortChords(d3.descending);
+
+    var arc = d3.svg.arc()
+        .innerRadius(innerRadius)
+        .outerRadius(innerRadius + 20);
+
+    var filepath;
+    if ($("#filename").data("filename") === "states") {
+      filepath = "/states.csv"
+    } else {
+      filepath = "/nhl.csv"
+    }
 
 
-  d3.csv("/data.csv", function(error, data) {
-    var states = data.map(function(d) { return d.state1 }).concat(data.map(function(d) { return d.state2 }));
+  d3.csv(filepath, type, function(error, data) {
 
-    var unique_states = d3.map(states, function(d) { return d; }).values();
+    var indexByState = d3.map();
+    var stateByIndex = d3.map();
+    var matrix = [];
+    var i = 0
 
-    var dataTree = {
-        children: unique_states.map(function(d) { return { name: d} } )
-    };
+    data.forEach(function(d) {
+      if (!indexByState.has(d.state1)) {
+        stateByIndex.set(i, d.state1);
+        indexByState.set(d.state1, i++);
+      }
+    });
+
+
+    data.forEach(function(d) {
+      var source = indexByState.get(d.state1),
+          row = matrix[source];
+      if (!row) {
+       row = matrix[source] = [];
+       for (var j = -1; ++j < i;) row[j] = 0;
+      }
+      row[indexByState.get(d.state2)] = +d.length;
+    });
+
+    chord.matrix(matrix);
+
 
     // size scale for data
 
     // determine the appropriate radius for the circle
-    var roughCircumference = 1600,
-        radius = roughCircumference / (Math.PI * 2);
-
-    var tree = d3.layout.tree()
-        .size([360, radius]);
-
-    var nodes = tree.nodes(dataTree);
-    // debugger
-
-    var circle = chart.selectAll("g")
-      .data(nodes.slice(1))
-    .enter().append("g")
-      .attr("transform", function(d) {
-        return "rotate(" + (d.x - 90) + ") translate(" + d.y + ")"; })
-
-    circle.append("circle")
-      .attr("class", "circle")
-      .attr("r", 10)
-
-    circle.append("text")
-      .attr("class", "circle")
-      .attr("text-anchor", "middle")
-      .attr("dx", "1.5em")
-      .text(function(d) { return d.name })
+    var g = svg.selectAll(".group")
+        .data(chord.groups)
+      .enter().append("g")
+        .attr("class", "group");
 
 
+    g.append("path")
+        .style("fill", function(d) { return fill(d.index); })
+        .style("stroke", function(d) { return fill(d.index); })
+        .attr("d", arc)
+        .on("mouseover", highlight)
+        .on("mouseout", highlightReset);
+
+    g.append("text")
+        .each(function(d) { d.angle = (d.startAngle + d.endAngle) / 2; })
+        .attr("dy", ".35em")
+        .attr("transform", function(d) {
+          return "rotate(" + (d.angle * 180 / Math.PI - 90) + ")"
+              + "translate(" + (innerRadius + 26) + ")"
+              + (d.angle > Math.PI ? "rotate(180)" : "");
+        })
+        .style("text-anchor", function(d) { return d.angle > Math.PI ? "end" : null; })
+        .text(function(d) { return stateByIndex.get(d.index); });
+
+    var chordPaths = svg.selectAll(".chord")
+        .data(chord.chords)
+      .enter().append("path")
+        .attr("class", "chord")
+        .attr("id", function(d) { return d.source.index; })
+        .style("stroke", function(d) { return d3.rgb(fill(d.source.index)).darker(); })
+        .style("fill", function(d) { return fill(d.source.index); })
+        .attr("d", d3.svg.chord().radius(innerRadius))
+        .on("mouseover", debug);
+
+
+    function highlight(d, i) {
+      chordPaths.classed("fade", function(p) {
+                    return p.source.index != i
+                        && p.target.index != i;
+                  });
+    }
+
+    function highlightReset(d, i) {
+      chordPaths.classed("fade", false);
+    }
   });
+
+  function type(d) {
+    d.length = +d.length;
+    return d;
+  }
+
+
+  function debug(d) {
+    // debugger;
+  }
 
 });
